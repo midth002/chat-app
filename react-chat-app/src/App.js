399c8eac0513fa0a514133ed1fd9cfd55c392565
Andrew Midthun <midth002@gmail.com>
2022-06-30 13:22:24 -0500

    Added to app.js

--- react-chat-app/src/App.js
+++ react-chat-app/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { ChatEngine } from 'react-chat-engine';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-    <ChatEngine 
-        height="100vh"
-        projectID="565aebda-4930-4f56-9584-8d8b72dde068"
-        userName="admin"
-        userSecret="@a88word"
-
-
-    />
+    <Router>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" component={Chat} />
+    </Router>
   );
 }
 
