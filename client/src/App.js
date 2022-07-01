9245f7f72d98fb289485430da454432d2f289862
Andrew Midthun <midth002@gmail.com>
2022-07-01 12:50:31 -0500

    Made socket io connection from backend to frontend. Adding users and room and messages to the backend.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/join/Join';
+import Chat from './components/chat/Chat';
 
 const App = () => {
   return (
--- server/server.js
+++ server/server.js
@@ -1,17 +1,57 @@
 const express = require('express');
-const socketio = require('socket.io');
+const { Server } = require('socket.io');
 const http = require('http');
+const cors = require('cors');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
 const PORT = process.env.PORT || 3001
 
+const app = express();
+
 const router = require('./router');
 
-const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    },
+});
+
+
 
 app.use(router);
 
 
-const io = socketio(server);
+io.on('connection', (socket) => {
+   
+    socket.on('join', ({name, room}, callback) => {
+       const { error, user } = addUser({id: socket.id, name, room});
+        if(error) {
+          return callback(error);
+        }
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`})
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`})
+        socket.join(user.room);
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message })
+
+        callback();
+    })
+
+    socket.on('disconnect', () => {
+        console.log('User has left.')
+    })
+})
 
 server.listen(PORT, () => console.log(`server has started on port ${PORT}`));
