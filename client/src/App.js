d90309dcd5d6f2cdf9b8b9d2e78de911a466ae93
Andrew Midthun <midth002@gmail.com>
2022-07-05 14:45:05 -0500

    Added to login to create a random id for room chats. Added hook use local storage to rerender the same id on the client side.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login'
+import Login from './components/Login/Login';
+import useLocalStorage from './hooks/useLocalStorage';
 
 
 const App = () => {
+
+  const [id, setId] = useLocalStorage()
+
   return (
-    <Login />
+    <>
+    {id}
+    <Login onIdSubmit={setId} />
+    </>
   );
 }
 
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { Container } from 'react-bootstrap'
+import React, { useRef } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import { v4 as uuidV4 } from 'uuid'
 
-const Login = () => {
+const Login = ({ onIdSubmit }) => {
+
+    const idRef = useRef();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        onIdSubmit(idRef.current.value);
+    }
+
+    function createNewId() {
+       onIdSubmit(uuidV4());
+    }
+
+
   return (
-    <Container>
-        Hello Login
+    <Container className="align-items-center d-flex" style={({height: '100vh'})}>
+        <Form className="w-100" onSubmit={handleSubmit}>
+        <Form.Group>
+            <Form.Label>Enter Your Id</Form.Label>
+            <Form.Control type="text" ref={idRef} required />
+        </Form.Group>
+        <Button type="submit" className='mr-2'>Login</Button>
+        <Button variant="secondary" onClick={createNewId}>Create a New Id</Button>
+        </Form>
     </Container>
   )
 }
