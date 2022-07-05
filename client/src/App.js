baa8ca646c87ca6fda19703b410e21110f65bf17
Andrew Midthun <midth002@gmail.com>
2022-07-05 14:22:29 -0500

    Rerending the app. Will be for doctors and patients to communicate via chat and video calls.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import Join from './components/join/Join';
-import Chat from './components/chat/Chat';
+import Login from './components/Login/Login'
+
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<Join /> }/>
-        <Route path="/chat" element={<Chat />} />
-        </Routes>
-    </Router>
+    <Login />
   );
 }
 
---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap'
+
+const Login = () => {
+  return (
+    <Container>
+        Hello Login
+    </Container>
+  )
+}
+
+export default Login
